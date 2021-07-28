8e672d4d73c7df28e3dcae87dbd589faf072e3f0
wagranungyo <waranungyo>
2021-07-28 13:43:16 +0900

    cnn model 추가

--- cnn_classifier.py
+++ cnn_classifier.py
@@ -1,3 +1,6 @@
 import tensorflow_datasets as tfds
-import tensorflow as tf
+import tensorflow_datasets as tfdf
+
+datasetm info = tfds.load("imdb_reviews/subwords8k",with_info = True, as_suprevised=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
 print(tf.__version__)
